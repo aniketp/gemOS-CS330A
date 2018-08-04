7df95d927fca2079d338775fd1d72af4c1c911f9
Aniket Pandey <aniketp@FreeBSD.org>
2018-08-04 12:06:29 +0530

    Add a gitignore for HW1 and original isPrime code

--- HW1/README.md
+++ HW1/README.md
@@ -4,6 +4,7 @@ This is a demontration of how `C` and `Assembly` functions can be called from ea
 ### Functionality
 * `isPrime()` is written in x86-64 assembly, and is called from main funtion in `main.c`
 * `find_square_root()` function, which is written in C, is called from `isPrime()`.
+* The assembly syntax is AT&T as GNU assembler does not originally support Intel Syntax
 
 ### Usage
 * Run `make` to compile and assemble the final executable. <br>
--- HW1/prime.s
+++ HW1/prime.s
@@ -1,3 +1,14 @@
+# "Equivalent C program"
+#
+# bool isPrime(int num) {
+#	 int sqroot = find_square_root(num);
+#	
+#	 for (int i = 2; i <= sqroot; i++)
+#		 if (!(num % i))
+#			 return false;
+#	 return true;
+# }
+
 .globl	isPrime
 
 isPrime:
@@ -10,15 +21,15 @@ isPrime:
 	call	find_square_root # Call the C function
 	movl	%eax, -4(%rbp)	# Store the return value
 	movl	$2, -8(%rbp)	# Initialize counter
-	movl	-8(%rbp), %eax	
+	movl	-8(%rbp), %eax	# Move the counter variable to EAX
 	cmpl	-4(%rbp), %eax	# Whether rbp - 4(sqroot) > counter
 	jle	.loop		# Loop condition still satisfies
 	movl	$1, %eax	# Return True
 
 .loop:
 	movl	-20(%rbp), %eax
-	cltd			# Fix for floating point exception
-	idivl	-8(%rbp)
+	cltd			# Extend the signed long bits of EAX to EDX
+	idivl	-8(%rbp)	# Divide EDX:EAX by the operator (num/i == 0)
 	movl	%edx, %eax	# IDIVL remainder stored in EDX
 	testl	%eax, %eax 	# Check if number is divisible
 	jne	.cont		# Continue if not equal
