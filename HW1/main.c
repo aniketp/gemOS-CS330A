1e4a5ebecbf0cad35b9d80525dba0ae43d5e8f0d
Aniket Pandey <aniketp@FreeBSD.org>
2018-08-10 21:49:07 +0530

    Lint fixes in HW1:main.c

--- HW1/main.c
+++ HW1/main.c
@@ -15,10 +15,12 @@ int main() {
 	int arr[10] = {123, 54, 1223, 12, 543, 1346, 655, 23, 65, 233};
 	int maxprime = 0;
 
-	for (int i = 0; i < 10; i++)
-		if (isPrime(arr[i]))
+	for (int i = 0; i < 10; i++) {
+		if (isPrime(arr[i])) {
 			if (maxprime < arr[i])
 				maxprime = arr[i];
+		}
+	}
 
 	printf("Maximum prime is: %d\n", maxprime);
 	return 0;
