925dd4527280be0ced9bee92f075e73567cb2f7d
Aniket Pandey <aniketp@FreeBSD.org>
2018-11-13 20:05:00 +0530

    This is 'some' progress :cry:

--- Assn4/objstore.c
+++ Assn4/objstore.c
@@ -15,43 +15,44 @@ typedef unsigned long long u64;
 typedef long long s64;
 #endif
 
-#define MAX_OBJS 1<<10
-#define HASH_SIZE 1<<10
-#define OBJECT_BLOCKS 1<<15
-
-
-////////////////////////
+#define MAX_OBJECT				1 << 20
 #define MAX_BLOCKS 				1 << 23
 #define MAX_INODES				1 << 20
+#define MAX_CACHES				1 << 15
 
-#define B_BLOCKS 				1 << 11
-#define I_BLOCKS				1 << 08
-#define C_BLOCKS 				1 << 08
-////////////////////////
+#define STRUCT_BLOCKS			21000
+
+#define B_BLOCKS 				1 << 8
+#define I_BLOCKS				1 << 5
+#define C_BLOCKS 				1 << 8
 
 u64 metadata_store = 0;
 u64 prev_meta_store = 0;
 
 struct object{
-	long id;
-	long size;
+	unsigned int id;
+	unsigned int size;
 	int cache_index;
 	int dirty;
 	char key[32];
 
-	struct object *next;
+	u32 level1[4];
+	u32 level2[4];	// Don't need more than 4000
+
+	// struct object *next;
 };
 
-struct multidx {
-	long level1[2];
-	long level2[2];
-};
+struct hashobj {
+	struct object obj;
+	struct hashobj *next;
+}
 
 struct object *objs;
-struct object *hash[MAX_OBJS] = {NULL};
+struct hashobj *hashTable[MAX_OBJECT] = {NULL};
 
-unsigned long *inodemap;
-unsigned long *blockmap;
+unsigned int inodemap[MAX_INODES/8];
+unsigned int blockmap[MAX_BLOCKS/8];		// Bitmap instead of Bytemap
+unsigned int cachemap[MAX_OBJECT];
 
 // Allocating 4k aligned memory area
 #define malloc_4k(x) do{\
@@ -59,14 +60,32 @@ unsigned long *blockmap;
                          if((x) == MAP_FAILED)\
                               (x)=NULL;\
                      }while(0);
+
+// Custom malloc
+#define malloc_y(x, size) do{\
+                      (x) = mmap(NULL, size * BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);\
+                      if((x) == MAP_FAILED)\
+                           (x)=NULL;\
+                  }while(0);
+
+
 #define free_4k(x) munmap((x), BLOCK_SIZE)
+#define free_y(x, y) munmap((x), y * BLOCK_SIZE)
 
 /*
 Returns the object ID.  -1 (invalid), 0, 1 - reserved
 */
 long find_object_id(const char *key, struct objfs_state *objfs)
 {
-    return -1;
+	u32 val = hash(key);
+	struct hashobj *itr = hashTable[val];
+
+	while (itr != NULL) {
+		if(itr->obj.id && (strcmp(itr->obj.key, key) == 0))
+			return itr->obj.id;
+		itr = itr->next;
+	}
+	return -1;
 }
 
 /*
@@ -78,10 +97,27 @@ long find_object_id(const char *key, struct objfs_state *objfs)
 */
 long create_object(const char *key, struct objfs_state *objfs)
 {
-	for (int ctr = 0; ctr < MAX_OBJS; ctr++) {
-		if ((inodemap[ctr/64] & (1 << (64 - ctr % 64))) == 0);
-	}
-	return -1;
+	int ctr;
+    struct object *obj = objs;
+    struct object *free = NULL;
+    for(ctr=0; ctr < MAX_OBJS; ++ctr){
+          if(obj->id == 0 && free == NULL){
+                free = obj;
+                free->id = ctr+2;
+          }
+          else if(obj->id && strcmp(obj->key, key) == 0){
+                return -1;		// Object already exists
+          }
+          obj++;
+    }
+
+    if(!free){
+               dprintf("%s: objstore full\n", __func__);
+               return -1;
+    }
+    strcpy(free->key, key);
+    init_object_cached(obj);
+    return free->id;
 }
 /*
   One of the users of the object has dropped a reference
@@ -153,18 +189,43 @@ int fillup_size_details(struct stat *buf)
 */
 int objstore_init(struct objfs_state *objfs)
 {
-	malloc_4k(blockmap);
+	struct object *obj = NULL;
+
+	// Make it a new malloc instead (LOL)
+	malloc_y(objs, STRUCT_BLOCKS);
+	if(!objs){		// NULL returned
+		dprintf("%s: malloc: objs structure\n", __func__);
+		return -1;
+	}
+
+	malloc_y(blockmap, B_BLOCKS);
     if(!blockmap){		// NULL returned
         dprintf("%s: malloc: blockmap\n", __func__);
         return -1;
     }
 
-	malloc_4k(inodemap);
+	malloc_y(inodemap, I_BLOCKS);
     if(!inodemap){		// NULL returned
         dprintf("%s: malloc: inodemap\n", __func__);
         return -1;
     }
 
+	for (int i = 0; i < STRUCT_BLOCKS; i++)
+		if(read_block(objfs, i, ((char *)objs) + (i * BLOCK_SIZE))) < 0)
+			return -1;
+
+	for (int i = 0; i < I_BLOCKS; i++)
+		if(read_block(objfs, STRUCT_BLOCKS + i, ((char *)inodemap) + (i * BLOCK_SIZE))) < 0)
+	        return -1;
+
+	for (int i = 0; i < B_BLOCKS; i++)
+		if(read_block(objfs, STRUCT_BLOCKS + I_BLOCKS + i, ((char *)blockmap) + (i * BLOCK_SIZE))) < 0)
+	        return -1;
+
+	for (int i = 0; i < MAX_OBJECT; i++)
+		cachemap[i] = -1;
+
+#if 0
 	// Respective bitmaps for blocks and inodes
 	if(read_block(objfs, 0, (char *)inodemap) < 0)
         return -1;
@@ -195,6 +256,8 @@ int objstore_init(struct objfs_state *objfs)
 	metadata_store = metadata_store + (OBJECT_BLOCKS);
 	free_4k(temp);
 	objfs->objstore_data = objs;
+#endif
+
 	dprintf("Done objstore init\n");
 	return 0;
 }
@@ -204,6 +267,29 @@ int objstore_init(struct objfs_state *objfs)
 */
 int objstore_destroy(struct objfs_state *objfs)
 {
+	struct object *obj = objs;
+	for(int ctr = 0; ctr < MAX_OBJECT; ctr++, obj++){
+			 if(obj->id)
+				 obj_sync(objfs, obj);
+	}
+
+	for (int i = 0; i < STRUCT_BLOCKS; i++)
+		if(write_block(objfs, i, ((char *)objs) + (i * BLOCK_SIZE))) < 0)
+			return -1;
+
+	for (int i = 0; i < I_BLOCKS; i++)
+		if(write_block(objfs, STRUCT_BLOCKS + i, ((char *)inodemap) + (i * BLOCK_SIZE))) < 0)
+			return -1;
+
+	for (int i = 0; i < B_BLOCKS; i++)
+		if(write_block(objfs, STRUCT_BLOCKS + I_BLOCKS + i, ((char *)blockmap) + (i * BLOCK_SIZE))) < 0)
+			return -1;
+
+	free_y(objs, STRUCT_BLOCKS);
+	free_y(inodemap, (I_BLOCKS));
+	free_y(blockmap, (B_BLOCKS));
+
+#if 0
 	metadata_store = metadata_store - (OBJECT_BLOCKS);
 
 	// Initialize
@@ -222,6 +308,8 @@ int objstore_destroy(struct objfs_state *objfs)
 	free_4k(inodemap);
 	free_4k(blockmap);
 	// free_4k(objfs);
+#endif
+
 	objfs->objstore_data = NULL;
 	dprintf("Done objstore destroy\n");
 	return 0;
